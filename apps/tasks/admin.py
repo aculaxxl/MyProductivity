06a79ea01105d2807661d10148e06210e51e2d11
Єлизавета <yelyzavetanovitska@MacBook-Air-Elizaveta.local>
2026-03-16 18:02:02 +0200

    feat: register Project, Tasks models in admin

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project, Task
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'priority', 'deadline', 'is_done')
+    list_filter = ('is_done', 'priority', 'project')
