dac4c7e1cf17db5c2674c42552abd9d5b9c4b03b
Єлизавета <yelyzavetanovitska@MacBook-Air-Elizaveta.local>
2026-03-16 21:28:45 +0200

    feat: add project creation with using HTMX and hyperscript

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Project
+from .forms import ProjectForm
+from django.urls import reverse_lazy
 
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
@@ -9,3 +12,20 @@ class ProjectListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Project.objects.filter(user=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ProjectForm() 
+        return context
+    
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    form_class = ProjectForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        project = form.save()
+        return render(self.request, 'tasks/project_element.html', {'project': project})
+
+    def get_success_url(self):
+        return reverse_lazy('tasks:project-list')
