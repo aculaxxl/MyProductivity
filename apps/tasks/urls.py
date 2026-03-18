e82d65336f39e324e262721635a4502390674fbb
Єлизавета <yelyzavetanovitska@MacBook-Air-Elizaveta.local>
2026-03-18 21:52:26 +0200

    Feat: added CRUD for projects

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import ProjectListView, ProjectCreateView, ProjectDetailView, TaskCreateView, TaskToggleView, TaskDeleteView, TaskEditView, TaskReorderView
+from .views import (ProjectListView, ProjectCreateView, 
+                    ProjectDetailView, TaskCreateView, 
+                    TaskToggleView, TaskDeleteView, 
+                    TaskEditView, TaskReorderView, 
+                    ProjectUpdateView, ProjectDeleteView,
+                    ProjectHeaderContentView)
 
 app_name = 'tasks'
 
@@ -12,4 +17,9 @@ urlpatterns = [
     path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
     path('tasks/reorder/', TaskReorderView.as_view(), name='task-reorder'),
     path('tasks/<int:pk>/edit/', TaskEditView.as_view(), name='task-edit'),
+    path('projects/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project-edit'),
+    path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project-delete'),
+    path('projects/<int:pk>/header/', ProjectHeaderContentView.as_view(), name='project-header-content'),
+
+
 ]
